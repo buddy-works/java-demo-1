27e2fb916d72c3123a39bc244f7a07708f4ae260
Darek <d.sztwiorok@gmail.com>
2019-03-13 16:53:32 +0100

    fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /
 ADD segfault-java.jar segfault-java.jar
 ADD lib/ lib/
 EXPOSE 8080
-CMD java -jar segfault-java.jar
+CMD ["java", "-jar", "segfault-java.jar"]
+
--- src/main/java/works/buddy/templates/CalcController.java
+++ src/main/java/works/buddy/templates/CalcController.java
@@ -1,10 +1,7 @@
 package works.buddy.templates;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import works.buddy.templates.CalcResult;
 import works.buddy.templates.Calculator;
 
@@ -15,22 +12,25 @@ public class CalcController {
     private Calculator calculator;
 
 
-
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, path = "/sum")
     public CalcResult sum(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
         return calculator.sum(a, b);
     }
 
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, path = "/subtract")
     public CalcResult subtract(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
         return calculator.subtract(a, b);
     }
 
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, path = "/multiply")
     public CalcResult multiply(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
         return calculator.multiply(a, b);
     }
 
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, path = "/divide")
     public CalcResult divide(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
         return calculator.divide(a, b);
